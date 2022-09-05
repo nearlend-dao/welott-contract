aef6a5f776df7330e8eef92279b70db503b7e2ea
MD <>
2022-09-05 22:16:03 +0700

    recognize folder

--- Nearlott/contract/src/callback.rs
+++
@@ -1,7 +0,0 @@
-use crate::*;
-use near_sdk::ext_contract;
-
-#[ext_contract(ext_ft_contract)]
-pub trait FungibleTokenCore {
-    fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>);
-}
