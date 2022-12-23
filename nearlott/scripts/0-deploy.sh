b876db5fa0f7906849fca9a86ebb0900c1d8e6e4
Lam NguyenSon <nguyensonlam@cryptopie-labo.com>
2022-12-23 22:43:19 +0700

    update scripts

--- nearlott/scripts/0-deploy.sh
+++ nearlott/scripts/0-deploy.sh
@@ -3,7 +3,7 @@ NETWORK=testnet
 SUFFIX=testnet
 
 MASTER_ACC=lamns1.$NETWORK
-CONTRACT_ACC=welott50.$MASTER_ACC
+CONTRACT_ACC=welott51.$MASTER_ACC
 OWNER=$MASTER_ACC
 TREASURY_ACC=lamns1.$NETWORK
 INJECTOR_ACC=lamns1.$NETWORK
@@ -16,38 +16,38 @@ export NEAR_ENV=$NETWORK
 # echo "################ DELETE THE ACCOUNT #########################"
 # near delete $CONTRACT_ACC $MASTER_ACC
 
-# echo "################ CREATE NEW ACCOUNT #########################"
-# near create-account $CONTRACT_ACC --masterAccount $MASTER_ACC --initialBalance 10
+echo "################ CREATE NEW ACCOUNT #########################"
+near create-account $CONTRACT_ACC --masterAccount $MASTER_ACC --initialBalance 10
 
-# echo "################ BUILD CONTRACT #########################"
-# ../build.sh
+echo "################ BUILD CONTRACT #########################"
+../build.sh
 
-# echo "################ DEPLOY CONTRACT #########################"
-# near deploy $CONTRACT_ACC ../out/nearlott.wasm
+echo "################ DEPLOY CONTRACT #########################"
+near deploy $CONTRACT_ACC ../out/nearlott.wasm
 
-# echo "################# INIT CONTRACT #########################"
-#  near call $CONTRACT_ACC --accountId=$OWNER new '{
-#      "owner_id":"'$OWNER'",
-#      "injector_address":"'$INJECTOR_ACC'",
-#      "operator_address": "'$OPERATOR_ACC'",
-#      "treasury_address": "'$TREASURY_ACC'",
-#      "config_lottery": {
-#         "time_run_lottery": 36000000000000,
-#         "price_ticket_in_near": "250000000000000000000000",
-#         "discount_divisor": "0",
-#         "rewards_breakdown": [125, 375, 750, 1250, 2500, 5000],
-#         "reserve_fee": "2200",
-#         "operate_fee": "500"
-#      }
-#  }'
+echo "################# INIT CONTRACT #########################"
+ near call $CONTRACT_ACC --accountId=$OWNER new '{
+     "owner_id":"'$OWNER'",
+     "injector_address":"'$INJECTOR_ACC'",
+     "operator_address": "'$OPERATOR_ACC'",
+     "treasury_address": "'$TREASURY_ACC'",
+     "config_lottery": {
+        "time_run_lottery": 36000000000000,
+        "price_ticket_in_near": "250000000000000000000000",
+        "discount_divisor": "0",
+        "rewards_breakdown": [125, 375, 750, 1250, 2500, 5000],
+        "reserve_fee": "2200",
+        "operate_fee": "500"
+     }
+ }'
 
 echo "####################### GET CONFIG LOTTERY #########################"
 near view $CONTRACT_ACC view_config_lottery ''
 
 near call $CONTRACT_ACC --accountId=$OWNER set_config_lottery '{
     "_config_lottery": {
-    "time_run_lottery": 84000000000000,
-    "price_ticket_in_near": "50000000000000000000000",
+    "time_run_lottery": 36000000000000,
+    "price_ticket_in_near": "250000000000000000000000",
     "discount_divisor": "0",
     "rewards_breakdown": [125, 375, 750, 1250, 2500, 5000],
     "reserve_fee": "2200",
