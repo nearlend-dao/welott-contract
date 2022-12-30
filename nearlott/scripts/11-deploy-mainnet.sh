a0370450fb6f7cf6bce17b6f3d3169c11575d87e
MD <duc@swing.xyz>
2022-12-30 22:09:17 +0700

    add new sh config files

---
+++ nearlott/scripts/11-deploy-mainnet.sh
@@ -0,0 +1,80 @@
+set -e
+NETWORK=near
+MASTER_ACC=welott.$NETWORK
+CONTRACT_ACC=beta1.$MASTER_ACC
+OWNER=nearlend-dao.sputnik-dao.near
+TREASURY_ACC=nearlend-dao.sputnik-dao.near
+INJECTOR_ACC=nearlend-dao.sputnik-dao.near
+OPERATOR_ACC=operator.$MASTER_ACC
+
+export NEAR_ENV=mainnet
+
+
+# echo "################ DELETE THE ACCOUNT #########################"
+# near delete $CONTRACT_ACC $MASTER_ACC
+
+# echo "################ CREATE NEW ACCOUNT #########################"
+# near create-account $CONTRACT_ACC --masterAccount $MASTER_ACC --initialBalance 5
+# near create-account $OPERATOR_ACC --masterAccount $MASTER_ACC --initialBalance 2
+
+# echo "################ BUILD CONTRACT #########################"
+# ../build.sh
+
+# echo "################ DEPLOY CONTRACT #########################"
+# near deploy $CONTRACT_ACC ../out/nearlott.wasm 
+
+# echo "####################### GET CONFIG LOTTERY #########################"
+# near view $CONTRACT_ACC view_config_lottery ''
+
+
+# # echo "################# INIT CONTRACT #########################"
+# near call $CONTRACT_ACC --accountId=$MASTER_ACC new '{
+#     "owner_id":"'$OWNER'",
+#     "injector_address":"'$INJECTOR_ACC'",
+#     "operator_address": "'$OPERATOR_ACC'",
+#     "treasury_address": "'$TREASURY_ACC'",
+#      "config_lottery": {
+#         "time_run_lottery": 43200000000000,
+#         "price_ticket_in_near": "250000000000000000000000",
+#         "discount_divisor": "0",
+#         "rewards_breakdown": [125, 375, 750, 1250, 2500, 5000],
+#         "reserve_fee": "2200",
+#         "operate_fee": "500"
+#      }
+# }'
+
+# echo "####################### GET CONFIG CONTRACT #########################"
+
+near view $CONTRACT_ACC get_config ''
+
+# near call $CONTRACT_ACC --accountId=$OWNER set_config_lottery '{
+#     "_config_lottery": {
+#     "time_run_lottery": 36000000000000,
+#     "price_ticket_in_near": "250000000000000000000000",
+#     "discount_divisor": "0",
+#     "rewards_breakdown": [125, 375, 750, 1250, 2500, 5000],
+#     "reserve_fee": "2200",
+#     "operate_fee": "500"
+#     }
+# }'
+
+#  near view $CONTRACT_ACC view_config_lottery ''
+
+# echo "####################### GET CONFIG CONTRACT #########################"
+# near view $CONTRACT_ACC --accountId=$CONTRACT_ACC view_random '' 
+
+# echo "######################## GET LATEST ID ROUND #########################"
+# near view $CONTRACT_ACC view_latest_lottery_id ''
+
+# echo "######################## GET DETAIL CURRENT ROUND #########################"
+# near view $CONTRACT_ACC view_current_lottery_running ''
+
+# echo "################# DEPOSIT STORAGE #########################"
+# near call $CONTRACT_ACC --accountId=$MASTER_ACC storage_deposit '{
+#     "account_id":"'$OWNER'"
+# }' --deposit=0.25
+
+
+# near call $CONTRACT_ACC --accountId=$MASTER_ACC storage_deposit '{
+#     "account_id":"'$OPERATOR_ACC'"
+# }' --deposit=0.25
---
+++ nearlott/scripts/12-round-mainnet.sh
@@ -0,0 +1,82 @@
+set -e
+NETWORK=near
+MASTER_ACC=welott.$NETWORK
+CONTRACT_ACC=beta1.$MASTER_ACC
+OWNER=nearlend-dao.sputnik-dao.near
+TREASURY_ACC=nearlend-dao.sputnik-dao.near
+INJECTOR_ACC=nearlend-dao.sputnik-dao.near
+OPERATOR_ACC=operator.$MASTER_ACC
+
+export NEAR_ENV=mainnet
+
+
+
+# echo "########################### GET CURRENT TIMESTAMP #########################"
+# near view $CONTRACT_ACC get_current_timestamp ''
+
+# # echo "######################## CLOSE ROUND #############################"
+#  near call $CONTRACT_ACC --accountId=$OWNER close_lottery '{}' --depositYocto=1
+
+# near call $CONTRACT_ACC --accountId=$OWNER draw_final_number_and_make_lottery_claimable '{
+#     "_lottery_id": 2,
+#     "_auto_injection": true
+# }' --depositYocto=1
+
+# near call $CONTRACT_ACC --accountId=$OWNER claim_tickets '{
+#     "_lottery_id": 1,
+#     "_ticket_ids": true
+# }' --depositYocto=1
+
+# echo "########################### START ROUND #########################"
+# # end_time=$(($(date +%s) + 24*60*60))
+# end_time=$(($(date +%s) + 1000*60))
+near call $CONTRACT_ACC --accountId=$OPERATOR_ACC start_lottery '' --depositYocto=1
+
+# echo "######################## BUY TICKETS #################################"
+# near call $CONTRACT_ACC --accountId=$OWNER buy_tickets '{
+#     "_lottery_id": 4,
+#     "_ticket_numbers":[1233145,1233146,1233141,1233142,1233143]
+# }' --depositYocto=499000000000000000000
+
+
+
+# echo "####################### GET CURRENT ROUND ID #########################"
+# near view $CONTRACT_ACC view_latest_lottery_id ''
+
+echo "######################## GET DETAIL CURRENT ROUND #########################"
+near view $CONTRACT_ACC view_current_lottery_running ''
+
+# echo "####################### GET CURRENT ROUND ID #########################"
+# near view $CONTRACT_ACC view_lottery '{
+#     "_lottery_id": 1
+# }'
+
+
+# echo "######################## CHECK PRICES FOR BUY TICKETS #################################"
+# near view $CONTRACT_ACC calculate_total_price_for_bulk_tickets '{
+#     "_lottery_id": 1,
+#     "_number_tickets": 5
+# }'
+
+# echo "######################## BUY TICKETS #################################"
+# near call $CONTRACT_ACC --accountId=$OWNER buy_tickets '{
+#     "_lottery_id": 2,
+#     "_ticket_numbers":[1233145,1233146,1233141,1233142,1233143]
+# }' --deposit=4.99
+
+# echo "######################## GET DETAIL CURRENT ROUND #########################"
+# near view $CONTRACT_ACC view_number_tickets_per_lottery '{
+#     "_lottery_id": 1
+# }'
+
+# echo "####################### VIEW TICKETS #################################"
+# near view $CONTRACT_ACC  view_user_info_for_lottery_id '{
+#     "_user": "'$OWNER'",
+#     "_lottery_id": 1,
+#     "_cursor": 0,
+#     "_size": 100
+# }' 
+
+
+# # echo "######################## CLOSE ROUND #############################"
+# near call $CONTRACT_ACC --accountId=$OWNER close_lottery '{}' --depositYocto=1
